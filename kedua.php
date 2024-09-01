c4ab7296617cda88f46616eb5d529ccf2a92e707
RioUsingGitHub <riomatin04@gmail.com>
2024-09-02 00:41:39 +0700

    Praktikum 4C - Membuat Constructor

--- kedua.php
+++ kedua.php
@@ -10,6 +10,11 @@ class BangunRuang {
 }
 
 class Lingkaran extends BangunRuang {
+
+    public function __construct($jari) {
+        parent::__construct($jari);
+    } 
+
     public function luasLingkaran() : float {
         return $this->phi * pow($this->jari, 2);
     }
@@ -20,6 +25,11 @@ class Lingkaran extends BangunRuang {
 }
 
 class Bola extends BangunRuang {
+
+    public function __construct($jari) {
+        parent::__construct($jari);
+    } 
+    
     public function volumeBola() : float {
         return (4/3) * $this->phi * pow($this->jari, 3);
     }
