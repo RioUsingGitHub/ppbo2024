7e1a21af7352c3900ccfab7198debcb676b8f492
RioUsingGitHub <riomatin04@gmail.com>
2024-09-02 02:15:27 +0700

    Praktikum 2a - Implementasi Input dan Output

--- inputoutput.php
+++ inputoutput.php
@@ -1,4 +1,20 @@
 <?php
-$nama = readline("Masukkan nama anda: ");
-echo "Halo, {$nama}";
+
+date_default_timezone_set("Asia/Jakarta");
+$nama = readline('Masukkan nama Anda: ');
+$jam = date('H');
+if ($jam >= 5 && $jam < 11) {
+    $status = "Pagi";
+}
+elseif ($jam >= 11 && $jam < 15) {
+    $status = "Siang";
+}
+elseif ($jam >= 15 && $jam < 19) {
+    $status = "Sore";
+}
+else {
+    $status = "Malam";
+}
+$menit = date('i');
+echo "Selamat {$status}, {$nama} sekarang pukul {$jam}:{$menit}\n";
 ?>
