705eeb2fe6c64413408da26e71d4152822db2d99
RioUsingGitHub <riomatin04@gmail.com>
2024-09-01 12:47:55 +0700

    Praktikum 2C - Function Sederhana

---
+++ kedua.php
@@ -0,0 +1,41 @@
+<?php
+
+class Rumus {
+
+    private $phi = 3.14;
+
+    public function luasLingkaran($jari) : float {
+        return $this->phi * pow($jari, 2);
+    }
+
+    public function kelilingLingkaran($jari) : float {
+        return 2 * $this->phi * $jari;
+    }
+
+    public function volumeBola($jari) : float {
+        return (4/3) * $this->phi * pow($jari, 3);
+    }
+
+    public function volumeTabung($jari, $tinggi) : float {
+        return $this->phi * pow($jari, 2) * $tinggi;
+    }
+
+    public function volumeKerucut($jari, $tinggi) : float {
+        return (1/3) * $this->phi * pow($jari, 2) * $tinggi;
+    }
+}
+
+$hitung = new Rumus();
+
+$luas_tanah = $hitung->luasLingkaran(45);
+$keliling_tanah = $hitung->kelilingLingkaran(45);
+$volume_bola = $hitung->volumeBola(45);
+$volume_tabung = $hitung->volumeTabung(45, 25);
+$volume_kerucut = $hitung->volumeKerucut(45, 25);
+
+echo "Luas tanah Budi adalah: {$luas_tanah}\n";
+echo "Keliling tanah Budi adalah: {$keliling_tanah}\n";
+echo "Volume bola adalah: {$volume_bola}\n";
+echo "Volume tabung adalah: {$volume_tabung}\n";
+echo "Volume kerucut adalah: {$volume_kerucut}\n";
+?>
