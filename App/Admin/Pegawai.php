3a53ee99a8f525a40fc76c063d344ec29f647a99
RioUsingGitHub <riomatin04@gmail.com>
2024-09-18 15:59:38 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Admin;
+
+class Pegawai
+{
+    public int $nip;
+    public string $nama;
+    protected int $no_hp;
+    public string $alamat;
+
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat)
+    {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+
+    public function cekIn(): bool
+    {
+        echo "{$this->nama} melakukan check-in\n";
+        return true;
+    }
+
+    public function cekOut(): bool
+    {
+        echo "{$this->nama} melakukan check-out\n";
+        return true;
+    }
+
+    public function getNoHp(): int
+    {
+        return $this->no_hp;
+    }
+
+    public function setNoHp(int $no_hp): void
+    {
+        $this->no_hp = $no_hp;
+    }
+}
