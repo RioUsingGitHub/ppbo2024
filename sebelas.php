a9d7acd63010267b393966aee4cb86a519e745d2
RioUsingGitHub <riomatin04@gmail.com>
2024-09-28 16:37:08 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- sebelas.php
+++ sebelas.php
@@ -4,5 +4,7 @@ require_once 'vendor/autoload.php';
 
 use App\Model\Akademik\Dosen;
 
-$dian = new Dosen("198411132015041001", "Dian Prawira", "62111111", "Jln Purnama", "0013118405");
+
+$dian = new Dosen("198411132015041001", "Dian Prawira", "62111111", "Jln Purnama", "1000121184001");
 $dian->mengajar();
+$dian->getNoHp();
